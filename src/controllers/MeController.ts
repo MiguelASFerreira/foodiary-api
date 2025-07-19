e742814bcd10080abf05cf1f8c579c866bb621d1
MiguelASFerreira <migant1306@gmail.com>
2025-07-19 15:12:58 -0300

    Adiciona endpoint para obter informações do usuário autenticado

---
+++ src/controllers/MeController.ts
@@ -0,0 +1,25 @@
+import { eq } from "drizzle-orm";
+import { db } from "../db/intex";
+import type { HttpResponse, ProtectedHttpRequest } from "../types/Http";
+import { ok } from "../utils/http";
+import { usersTable } from "../db/schema";
+
+
+export class MeController {
+  static async handle({ userId }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        name: true,
+        email: true,
+        calories: true,
+        proteins: true,
+        carbohydrates: true,
+        fats: true,
+      },
+      where: eq(usersTable.id, userId)
+    })
+
+    return ok({ user})
+  }
+}
