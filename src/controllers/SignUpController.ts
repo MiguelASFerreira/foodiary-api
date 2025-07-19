360899bea7253787ed6e6b0d6ddb67a1504fd620
MiguelASFerreira <migant1306@gmail.com>
2025-07-19 11:27:20 -0300

    Validações com Zod e correção do serverless

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,10 +1,33 @@
+import { z } from "zod";
 import type { HttpRequest, HttpResponse } from "../types/Http";
-import { created } from "../utils/http";
+import { badRequest, created } from "../utils/http";
+
+const schema = z.object({
+  goal: z.enum(["lose", "maintain", "gain"]),
+  gender: z.enum(["male", "female"]),
+  birthDate: z.iso.date(),
+  weight: z.number(),
+  height: z.number(),
+  activityLevel: z.number().min(1).max(5),
+  account: z.object({
+    name: z.string().min(1),
+    email: z.email(),
+    password: z.string().min(8),
+  })
+})
 
 export class SignUpController {
-  static async handle(request: HttpRequest): Promise<HttpResponse> {
+  static async handle({ body }: HttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(body)
+    
+    if (!success) {
+      return badRequest({
+        errors: error.issues
+      })
+    }
+
     return created({
-      accessToken: "token de acesso",
+      data
     })
   }
 }
