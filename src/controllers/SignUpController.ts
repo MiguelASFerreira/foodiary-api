b33040509735d1db650187517ba998337e11758a
MiguelASFerreira <migant1306@gmail.com>
2025-07-19 12:25:14 -0300

    Salvar usuario no db

--- src/controllers/SignUpController.ts
+++ src/controllers/SignUpController.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import type { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, created } from "../utils/http";
+import { badRequest, conflict, created } from "../utils/http";
+import { db } from "../db/intex";
+import { usersTable } from "../db/schema";
+import { eq } from "drizzle-orm";
+import type id from "zod/v4/locales/id.cjs";
 
 const schema = z.object({
   goal: z.enum(["lose", "maintain", "gain"]),
@@ -26,8 +30,38 @@ export class SignUpController {
       })
     }
 
+    const userAlreadyExists = await db.query.usersTable.findFirst({
+      columns: {
+        email: true
+      },
+      where: eq(usersTable.email, data.account.email)
+    });
+
+    if (userAlreadyExists) {
+      return conflict({
+        error: 'This email is already registered.'
+      })
+    }
+
+
+    const { account, ...rest } = data
+
+    const [user] = await db.insert(usersTable).values({
+      name: account.name,
+      email: account.email,
+      password: account.password,
+      ...rest,
+      calories: 0,
+      carbohydrates: 0,
+      fats: 0,
+      proteins: 0,
+    }).returning({
+      id: usersTable.id,
+    })
+
+
     return created({
-      data
+      userId: user.id
     })
   }
 }
