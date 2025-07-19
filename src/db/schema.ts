8adb57879977322b30adfe529a490e9d7229255d
MiguelASFerreira <migant1306@gmail.com>
2025-07-19 12:04:22 -0300

    Users schema

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import { pgTable, uuid, varchar, date, integer } from "drizzle-orm/pg-core";
+import { email } from "zod";
+
+export const usersTable = pgTable("users", {
+  id: uuid().primaryKey().defaultRandom(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).unique().notNull(),
+  password: varchar({ length: 255 }).notNull(),
+  goal: varchar({ length: 8 }).notNull(),
+  gender: varchar({ length: 6 }).notNull(),
+  birthDate: date('birth_date').notNull(),
+  height: integer().notNull(),
+  weight: integer().notNull(),
+  activityLevel: integer('activity_level').notNull(),
+
+  // Goals
+  calories: integer().notNull(),
+  proteins: integer().notNull(),
+  carbohydrates: integer().notNull(),
+  fats: integer().notNull(),
+})
